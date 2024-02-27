b60cb71ba737a317b211a9d72464be8252ca1cb5
Rafael Machado <rafmco2@hotmail.com>
2024-02-26 20:34:35 -0400

    Adicionado Enums;

---
+++ src/enums/CandleColor.ts
@@ -0,0 +1,8 @@
+// Comportamento da Cotação, aumento de valor, queda ou não variante
+enum CandleColor {
+  GREEN = 'gree',
+  RED = 'red',
+  UNDETERMINED = 'undetermined'
+}
+
+export default CandleColor
