bea0fdf16d8a7de7d300dbe997c1c0bb93d66ad9
Rafael Machado <rafmco2@hotmail.com>
2024-02-28 15:33:34 -0400

    Fix: removido apexcharts dos imports globais;

--- bitcoin-candle-app/src/main.ts
+++ bitcoin-candle-app/src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
-import VueApexCharts from "vue3-apexcharts"
 import App from './App.vue'
 import store from './store'
 
 createApp(App)
   .use(store)
-  .use(VueApexCharts)
   .mount('#app')
