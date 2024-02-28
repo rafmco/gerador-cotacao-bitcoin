d2046e5d94865a4ade4f89441f02c6628d44a3fe
Rafael Machado <rafmco2@hotmail.com>
2024-02-27 22:41:22 -0400

    Add socket e toast no App.Vue;

--- bitcoin-candle-app/src/App.vue
+++ bitcoin-candle-app/src/App.vue
@@ -9,11 +9,14 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import { getModule } from 'vuex-module-decorators'
+import io from 'socket.io-client'
+import { createToast } from 'mosha-vue-toastify'
 import 'mosha-vue-toastify/dist/style.css'
 import CandleStickChart from './components/CandleStickChart.vue'
 import Header from './components/Header.vue'
 import store from './store'
 import CandleStore from './store/modules/CandleStore'
+import Candle from './models/Candle'
 
 @Options({
   components: {
@@ -25,8 +28,16 @@ export default class App extends Vue {
 
   candleStore = getModule(CandleStore, store)
 
+  socket = io(process.env.VUE_APP_SOCKET_SERVER ?? '')
+
   mounted() {
     this.candleStore.loadInitialCandles()
+
+    this.socket.on(process.env.VUE_APP_SOCKET_EVENT_NAME ?? '', (msg: object) => {
+      const candle = new Candle(msg)
+      this.candleStore.addCandle(candle)
+      createToast('New candle arrived!', { type: 'info' })
+    })
   }
 
   get candles() {
